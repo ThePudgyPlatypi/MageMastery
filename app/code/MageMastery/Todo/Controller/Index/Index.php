81e206675f1a601b23aee609502716df8127b0a4
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-09-19 00:14:30 -0400

    lesson 5

--- app/code/MageMastery/Todo/Controller/Index/Index.php
+++ app/code/MageMastery/Todo/Controller/Index/Index.php
@@ -1,31 +1,12 @@
 <?php
-namespace MageMastery\Todo\Controller;
+namespace MageMastery\Todo\Controller\Index;
 
-class Index extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
 {
-    /**
-     * @var \Magento\Framework\View\Result\PageFactory
-     */
-    protected $_pageFactory;
-
-    /**
-     * @param \Magento\Framework\App\Action\Context $context
-     */
-    public function __construct(
-       \Magento\Framework\App\Action\Context $context,
-       \Magento\Framework\View\Result\PageFactory $pageFactory
-    )
-    {
-        $this->_pageFactory = $pageFactory;
-        return parent::__construct($context);
-    }
-    /**
-     * View page action
-     *
-     * @return \Magento\Framework\Controller\ResultInterface
-     */
-    public function execute()
-    {
-        return $this->_pageFactory->create();
+    public function execute() {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
