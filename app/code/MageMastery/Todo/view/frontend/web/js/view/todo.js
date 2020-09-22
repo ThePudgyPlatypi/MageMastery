771b12d8169fa0de37ac718971f88fb13e7c8a7d
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-09-21 22:12:57 -0400

    lesson 16

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,5 +1,13 @@
 define(['uiComponent'], function(Component) {
     'use strict'
     console.log("UI component is loading");
-    return Component.extend({});
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {label: "Task 4"},
+                {label: "Task 5"},
+                {label: "Task 6"}
+            ]
+        }
+    });
 })
