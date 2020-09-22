928dad60abcd40252e41e007867a7ca2d1d00350
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-09-21 21:09:31 -0400

    lesson 14

--- app/etc/config.php
+++ app/etc/config.php
@@ -306,7 +306,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
-        'Ced_DevTool' => 1,
+        'Ced_DevTool' => 0,
         'Custom_FullPage' => 0,
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
