efb22e22809743915ca6668a1a4409fff9fbf677
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-10-11 01:19:41 -0400

    Lesson 32

--- app/code/MageMastery/Todo/Controller/Index/Index.php
+++ app/code/MageMastery/Todo/Controller/Index/Index.php
@@ -3,10 +3,33 @@ namespace MageMastery\Todo\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Action\Context;
+use MageMastery\Todo\Model\Task;
+use MageMastery\Todo\Model\TaskFactory;
+use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
 
 class Index extends Action
 {
+    private $taskFactory;
+    private $taskResource;
+
+    public function __construct(Context $context, TaskResource $taskResource, TaskFactory $taskFactory) {
+        $this->taskFactory = $taskFactory;
+        $this->taskResource = $taskResource;
+
+        parent::__construct($context);
+    }
+
     public function execute() {
+        $task = $this->taskFactory->create();
+        $task->setData([
+            'label' => 'Test Task',
+            'status' => 'open',
+            'customer_id' => 1
+        ]);
+
+        $this->taskResource->save($task);
+
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
--- app/code/MageMastery/Todo/Model/Task.php
+++ app/code/MageMastery/Todo/Model/Task.php
@@ -1,12 +1,12 @@
 <?php
 namespace MageMastery\Todo\Model;
 
-use MageMastery\Model\ResourceModel\Task as TaskResouce;
+use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
 
 class Task extends \Magento\Framework\Model\AbstractModel
 {
     protected function _construct() {
-        $this->_init(TaskResouce::class);
+        $this->_init(TaskResource::class);
     }
     
 }
