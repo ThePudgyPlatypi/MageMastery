9c6fa288ff2ba51f3b90c13a7a4dbbc2934ef73d
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-10-13 22:24:28 -0400

    Lesson 34

---
+++ app/code/MageMastery/Todo/Api/Data/TaskSearchResultsInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Todo\Api\Data;
+
+use Magento\Framework\Api\SearchResultsInterface;
+
+interface TaskSearchResultsInterface extends SearchResultsInterface
+{
+    /**
+     *
+     * @return TaskInterface[]
+     */
+    public function getItems();
+
+    /**
+     * @param TaskInterface[]
+     * @return SearchResultsInterface
+     */
+
+    public function setItems(array $items);
+}
--- app/code/MageMastery/Todo/Api/TaskRepositoryInterface.php
+++ app/code/MageMastery/Todo/Api/TaskRepositoryInterface.php
@@ -1,13 +1,16 @@
 <?php
 namespace MageMastery\Todo\Api;
 
+use Magento\Framework\Api\SearchCriteriaInterface;
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+
 /**
  * @api
  */
 
 interface TaskRepositoryInterface
 {
-    public function getList();
+    public function getList(SearchCriteriaInterface $seachCriteria): TaskSearchResultInterface;
     public function get(int $taskId);
 
 }
