3dd1d9b6da70fbc211c963245567f62fb78460c8
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-10-13 23:08:44 -0400

    Lesson 36

--- app/code/MageMastery/Todo/Service/TaskRepository.php
+++ app/code/MageMastery/Todo/Service/TaskRepository.php
@@ -4,16 +4,28 @@ namespace MageMastery\Todo\Service;
 use MageMastery\Todo\Api\TaskRepositoryInterface;
 use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
 use MageMastery\Todo\Model\TaskFactory;
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use MageMastery\Todo\Api\Data\TaskSearchResultInterfaceFactory;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
 
 class TaskRepository implements TaskRepositoryInterface
 {
     private $taskResource;
     private $taskFactory;
+    private $searchResultsFactory;
+    private $collectionProcessor;
 
-    public function __construct(TaskResource $taskResource, TaskFactory $taskFactory)
+    public function __construct(
+        TaskResource $taskResource, 
+        TaskFactory $taskFactory,
+        CollectionProcessorInterface $collectionInterface,
+        TaskSearchResultInterfaceFactory $searchResultFactory)
     {
         $this->taskResource = $taskResource;
         $this->taskFactory = $taskFactory;
+        $this->collectionProcessor = $collectionProcessor;
+        $this->searchResultsFactory = $searchResultFactory;
     }
 
     public function get(int $taskId) {
@@ -22,7 +34,11 @@ class TaskRepository implements TaskRepositoryInterface
         return $object;
     }
 
-    public function getList() {
+    public function getList(SearchCriteriaInterface $searchCriteria): TaskSearchResultInterface {
+        $searchResult = $this->searchResultsFactory->create();
+        $searchResult->setSearchCriteria($searchCriteria);
+        $this->collectionProcessor->process($searchCriteria, $searchResult);
 
+        return $searchResult;
     }
 }
