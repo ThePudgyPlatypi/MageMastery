e1c2a2d42c55d1c9117eab41c390732f11b8fe83
ThePudgyPlatypi <stehmc1@tcnj.edu>
2020-10-19 21:46:15 -0400

    lesson37

--- app/code/MageMastery/Todo/Model/ResourceModel/Task/Collection.php
+++ app/code/MageMastery/Todo/Model/ResourceModel/Task/Collection.php
@@ -1,21 +1,54 @@
 <?php
-// namespace MageMastery\Todo\Model\ResourceModel\Task;
+namespace MageMastery\Todo\Model\ResourceModel\Task;
 
-// use MageMastery\Todo\Model\Task;
-// use MageMaster\Todo\Model\ResourceModel\Task as TaskResource;
-// use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
-// use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use MageMastery\Todo\Model\Task;
+use MageMaster\Todo\Model\ResourceModel\Task as TaskResource;
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use Magento\framework\Api\SearchCriteriaInterface;
 
-// class Collection extends AbstractCollection implements TaskSearchResultInterface
-// {
-//     /**
-//      * Define the resource model & the model.
-//      *
-//      * @return void
-//      */
-//     protected function _construct()
-//     {
-//         $this->_init(Task::class, TaskResouce::class);
-//     }
+class Collection extends AbstractCollection implements TaskSearchResultInterface
+{
+    private $searchCriteria;
 
-// }
+    /**
+     * Define the resource model & the model.
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(Task::class, TaskResource::class);
+    }
+
+
+    public function setItems(array $items = null) {
+        if (!$items) {
+            return $this;
+        }
+
+        foreach($item as $items) {
+            $this->addItem($item);
+        }
+        
+        return $this;
+    }
+
+    public function getSearchCriteria() {
+        return $this->searchCriteria;
+    }
+
+    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria = null) {
+        $this->searchCriteria = $searchCriteria;
+        return $this;
+    }
+
+    public function getTotalCount() {
+        return $this->getSize();
+    }
+
+
+    public function setTotalCount() {
+        return $this;
+    }
+}
